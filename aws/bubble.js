793fa86245072f4079a201b246f9b2f6bfdd7961
Andrew Huang <andrevv10@users.noreply.github.com>
2018-06-03 01:36:53 -0700

    Update bubble.js

--- aws/bubble.js
+++ aws/bubble.js
@@ -21,7 +21,7 @@ exports.handler = (event, context, callback) => {
       }
       return array;
     }
-    console.log(bubbleSortBasic(array.slice())); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+    bubbleSortBasic(array.slice()); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
     var end = (new Date().getTime() - start);
     callback(null, end.toString());
 };
